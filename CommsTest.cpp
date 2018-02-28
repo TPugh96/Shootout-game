35905fd48fa8e3c905662bfc95068584c39b92e3
JoshRamini <Joshua2.Ramini@live.uwe.ac.uk>
2018-02-28 00:46:43 +0000

    Completed working commstest

--- CommsTest.cpp
+++ CommsTest.cpp
@@ -3,110 +3,58 @@
 MicroBit uBit;
 MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
 MicroBitPin P1(MICROBIT_ID_IO_P1, MICROBIT_PIN_P1, PIN_CAPABILITY_ALL);
-
 int binary [3] = {0,0,0};
 
-
-
-void sendMessage(){
-  P0.setDigitalValue(1);
-  uBit.display.print("C");
-  uBit.sleep(300);
-  //uBit.sleep(20);
-  P0.setDigitalValue(0);
-}
-
-void sendBinary(MicroBitEvent){
-  P0.setDigitalValue(1);
-  wait_us(100000);
-  P0.setDigitalValue(0);
-  wait_us(100000);
-  P0.setDigitalValue(0);
-  wait_us(100000);
-  P0.setDigitalValue(1);
-}
-
-void receiveBinary(){
-  if (P1.getDigitalValue() == 1){
-    wait_us(100000);
-    binary[0] = P1.getDigitalValue();
-    wait_us(100000);
-    binary[1] = P1.getDigitalValue();
-    wait_us(100000);
-    binary[2] = P1.getDigitalValue();
-    uBit.sleep(100);
-  }
-}
-
-void commsTest(){
-
-  //sendBinary();
-  //P0.setDigitalValue(0);
-  //create_fiber(receiveBinary);
-
-
-
-
-  //int binary [3] = {0,0,0};
-
-  uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, sendBinary);
-  //uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, sendMessage);
-
-  //while (true){
-
-
-
-    /*if (uBit.buttonA.isPressed() == 1){
-      sendBinary();
-    }*/
-    /*if (P1.getDigitalValue() == 1){
-      wait_us(100000);
+void listen(){
+  while (true){
+    if (P1.getDigitalValue() == 1){
+      wait_us(500);
       binary[0] = P1.getDigitalValue();
-      wait_us(100000);
+      wait_us(500);
       binary[1] = P1.getDigitalValue();
-      wait_us(100000);
+      wait_us(500);
       binary[2] = P1.getDigitalValue();
-      wait_us(100000);
-
-
-      /*for (int k = 0 ; k < 3 ; k++){
-        uBit.display.print(binary[k]);
-        wait_us(100000);
-        uBit.display.clear();
-      }*/
-      //uBit.display.print("Y");
-      //uBit.sleep(300);
-    //} else {
-      //uBit.display.print("X");
-      //uBit.sleep(300);
-    //}
-  //}
+      wait_us(500);
+    }
+    uBit.sleep(1);
+  }
 }
 
+void send(MicroBitEvent){
+  //Will be turned into a multi use function with paramenters. Currently used for testing.
 
-void send(){
   P0.setDigitalValue(1);
-  uBit.display.print("X");
+  wait_us(500);
+  P0.setDigitalValue(0);
+  wait_us(500);
+  P0.setDigitalValue(0);
+  wait_us(500);
+  P0.setDigitalValue(1);
+  wait_us(500);
+
+  //Reset
+  //P0.setDigitalValue(0);
 }
 
-void receive(){
-  if (P1.getDigitalValue() == 1){
-    uBit.display.print("Y");
+void runProgram(){
+
+  create_fiber(listen);
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, send, MESSAGE_BUS_LISTENER_IMMEDIATE);
+
+  while (true){
+    uBit.sleep(1);
+    uBit.display.print(binary[2]);
+    //uBit.display.print(P1.getDigitalValue());
   }
 }
 
 
-
-int main(){
+int main (){
   uBit.init();
   P0.setDigitalValue(0);
   P1.setDigitalValue(0);
 
-  while (true){
-    commsTest();
-    //send();
-    //receive();
+  while(true){
+    runProgram();
   }
-
-
 }
