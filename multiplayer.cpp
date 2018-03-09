80cb1b4f724c8f5de3eebe2380dad8e156ef284a
JoshRamini <Joshua2.Ramini@live.uwe.ac.uk>
2018-03-09 16:46:14 +0000

    Program almost done. Completed win/lose

--- multiplayer.cpp
+++ multiplayer.cpp
@@ -14,8 +14,6 @@ struct Point
 MicroBit uBit;
 MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
 MicroBitPin P1(MICROBIT_ID_IO_P1, MICROBIT_PIN_P1, PIN_CAPABILITY_ALL);
-int binary[3];
-int x_value;
 
 //Encryption Stuff
 int sendIndex = 0;
@@ -29,17 +27,9 @@ Point bullet;
 Point incomingBullet;
 int PLAYER_SPEED = 150;
 int BULLET_SPEED = 100;
-int game_over;
-int playerScore = 0;
-
-void incomingFire(int bulletX)
-{
-    if (incomingBullet.y == -1)
-    {
-        incomingBullet.x = bulletX;
-        incomingBullet.y = 0;
-    }
-}
+bool game_over;
+int hitsTaken;
+bool winner;
 
 uint8_t getBits() {
     // Tell server we'd like some data. Pretty please. :-)
@@ -107,17 +97,22 @@ void incomingBulletUpdate() {
     while (!game_over)
     {
         uBit.sleep(BULLET_SPEED);
-        if (incomingBullet.y != -1)
-        {
-            incomingBullet.y++;
+        if (incomingBullet.y < 5){
+          incomingBullet.y++;
+        }
+
+        //Checking if the incoming bullet hits the players location. Icrements
+        //the hits taken.
+        if ((incomingBullet.x == player.x) & (incomingBullet.y == player.y)){
+          hitsTaken++;
         }
 
         //If bullet goes out of bounds it is reset until fire is triggered again
         if (shootout.getPixelValue(incomingBullet.x, incomingBullet.y) > 0)
         {
             shootout.setPixelValue(incomingBullet.x, incomingBullet.y, 0);
-            incomingBullet.x = -1;
-            incomingBullet.y = -1;
+            incomingBullet.x = 5;
+            incomingBullet.y = 5;
         }
     }
 }
@@ -139,13 +134,13 @@ void bulletUpdate()
     while (!game_over)
     {
         uBit.sleep(BULLET_SPEED);
-
         if (bullet.y > -1){
           bullet.y--;
         }
 
         if (bullet.y == 0){
-          sendBits(bullet.x);
+          //We're sending the x coord + 1 to negate the issue of 0.
+          sendBits(bullet.x + 1);
           bullet.x = -1;
         }
 
@@ -159,46 +154,63 @@ void bulletUpdate()
     }
 }
 
-//Checks whether there is a shield tile at a certain LED, returns 1 if there is
-/*int checkShield (int x, int y)
-{
-    for(int i = 0; i < 4; i++)
-    {
-        if (shootout.getPixelValue(x, y) == 255)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-}
-*/
-
 //Adds 3 shield tiles centered above the player, these can block bullets
-/*void shield(MicroBitEvent)
+void shield(MicroBitEvent)
 {
     for(int x = player.x - 1; x < player.x + 2; x++)
     {
         shootout.setPixelValue(x, 3, 255);
     }
-}*/
+}
 
 void listen(){
   while (!game_over){
     uBit.sleep(1);
+    //The values are inverted as to replicate the game as if the microbits are
+    //being held facing eachother.
     switch ((int)getBits()){
-      case 0: incomingFire(0);
+      case 1: incomingBullet.y = 0;
+              incomingBullet.x = 4;
       break;
-      case 1: incomingFire(1);
+      case 2: incomingBullet.y = 0;
+              incomingBullet.x = 3;
       break;
-      case 2: incomingFire(2);
+      case 3: incomingBullet.y = 0;
+              incomingBullet.x = 2;
       break;
-      case 3: incomingFire(3);
+      case 4: incomingBullet.y = 0;
+              incomingBullet.x = 1;
       break;
-      case 4: incomingFire(4);
+      case 5: incomingBullet.y = 0;
+              incomingBullet.x = 0;
       break;
+      case 6: winner = true;
+              game_over = true;
+      break;
+    }
+  }
+}
+
+void gameOver(){
+  if (winner){
+    for (int k = 0 ; k < 2 ; k++){
+      uBit.display.scroll("YOU WIN!");
+    }
+  } else {
+    for (int k = 0 ; k < 2 ; k++){
+      uBit.display.scroll("YOU LOSE!");
+    }
+  }
+}
+
+//Game over function.
+void checkGameOver()
+{
+  while (!game_over){
+    uBit.sleep(1);
+    if (hitsTaken == 3){
+      sendBits(6);
+      game_over = true;
     }
   }
 }
@@ -206,23 +218,26 @@ void listen(){
 //Main game function, sets original attributes and triggers most functions
 void shootoutGame()
 {
-    game_over = 0;
+    game_over = false;
+    winner = false;
+    hitsTaken = 0;
     player.x = 2;
     player.y = 4;
     bullet.x = -1;
     bullet.y = -1;
-    incomingBullet.x = -1;
-    incomingBullet.y = -1;
+    incomingBullet.x = 5;
+    incomingBullet.y = 5;
 
     //Event handlers for button presses, triggers bullet and shield
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, fire);
-    //uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, shield);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, shield);
 
     //Individual fibers for moving elements of the game, prevents freezing and general bugginess
     create_fiber(listen);
     create_fiber(incomingBulletUpdate);
     create_fiber(playerUpdate);
     create_fiber(bulletUpdate);
+    create_fiber(checkGameOver);
 
     //refreshes the screen preventing duplicates of screen elements
     while (!game_over)
@@ -231,18 +246,15 @@ void shootoutGame()
         uBit.display.image.paste(shootout);
         uBit.display.image.setPixelValue(player.x, player.y, 255);
         uBit.display.image.setPixelValue(bullet.x, bullet.y, 255);
+        uBit.display.image.setPixelValue(incomingBullet.x, incomingBullet.y, 255);
     }
-
-    //if(game_over == 1)
-        //gameOver();
-
+    gameOver();
 }
 
 int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
-    //uBit.display.setDisplayMode(DISPLAY_MODE_BLACK_AND_WHITE);
 
     //Title message
     uBit.display.scroll("SHOOTOUT!");
